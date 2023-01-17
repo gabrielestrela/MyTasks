45a476335e811467ee3423441db22b9a1583d608
gabriel-estrela <gabrielestrela92@gmail.com>
2023-01-16 22:28:58 -0300

    feature: add flipper

--- app/src/main/java/com/star/mytasks/customapp/MyTasksApp.kt
+++ app/src/main/java/com/star/mytasks/customapp/MyTasksApp.kt
@@ -1,6 +1,12 @@
 package com.star.mytasks.customapp
 
 import android.app.Application
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.soloader.SoLoader
 import com.star.core.BuildConfig
 import com.star.core.log.CrashReportTree
 import com.star.core.moshi.di.moshiModule
@@ -17,12 +23,30 @@ class MyTasksApp : Application() {
             androidContext(this@MyTasksApp)
             modules(appModules)
         }
-
+        initializeFlipper()
         Timber.plant(getTimberTree())
     }
 
     private fun getTimberTree(): Timber.Tree =
         if (BuildConfig.DEBUG) Timber.DebugTree() else CrashReportTree()
+
+    private fun initializeFlipper() {
+        SoLoader.init(this, false)
+
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            val client = AndroidFlipperClient.getInstance(this)
+            with(client) {
+                addPlugin(
+                    InspectorFlipperPlugin(
+                        this@MyTasksApp,
+                        DescriptorMapping.withDefaults()
+                    )
+                )
+                addPlugin(DatabasesFlipperPlugin(this@MyTasksApp))
+            }
+            client.start()
+        }
+    }
 
     private val appModules: List<Module> = listOf(
         moshiModule
